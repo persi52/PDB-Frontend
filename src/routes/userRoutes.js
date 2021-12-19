281957da506b7d4cb1207fb05f336c501395cd2e
Bartlomiej Zima <bartlomiej.zima@o2.pl>
2021-12-19 19:55:15 +0100

    token

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -25,7 +25,7 @@ export function getUsers(){
 
 export function signOut(){
   document.cookie = "token=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
-  window.location.href="/";
+  //window.location.href="/";
   authApi.delete('/signOut');
 }
 
