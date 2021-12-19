3edf4c72290fb0af8cbfbc97d5867da247204ce1
Bartlomiej Zima <bartlomiej.zima@o2.pl>
2021-12-19 17:41:44 +0100

    search , responssivness

--- src/routes/commentRoute.js
+++ src/routes/commentRoute.js
@@ -3,6 +3,11 @@ require('dotenv').config();
 
 const backendURL = process.env.REACT_APP_BACKEND_URL;
 
+const commentsApi = axios.create({
+    baseURL: backendURL + 'comments',
+    withCredentials: true
+})
+
 
 export async function getComments(movie_id) { 
     let data = await commentsApi.get(`/get/`+movie_id).then(({data}) => data);
