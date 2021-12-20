2f5a2ed528614506df944499d348c45230cd44a4
Bartlomiej Zima <bartlomiej.zima@o2.pl>
2021-12-20 15:51:47 +0100

    token

--- src/components/Navbar_logged.jsx
+++ src/components/Navbar_logged.jsx
@@ -18,7 +18,7 @@ export function Navbar_logged(user_id) {
                 <a href="/favourities" className='nav-link'>Moja lista</a>
                 <a href="/notifications" className='nav-link'>Powiadomienia</a>
                 <a href={`/myprofile`} className='nav-link' >Mój profil</a>  
-                <a onClick={() => signOut()} classname='nav-link' href="/">Wyloguj się</a>
+                <a onClick={signOut()} classname='nav-link' href="/">Wyloguj się</a>
             </div>
             
         </div>
